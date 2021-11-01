d7da19855463f6e83d3a18beff01fad51ae9e4d2
Martín Suárez <ms.prgramador@gmail.com>
2021-11-01 23:33:12 +0000

    creado la page Login y su contenedor LoginContainer

---
+++ src/front/js/component/loginContainer.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const LoginContainer = () => {
+    return (
+    <Fragment>
+        {/* aca va el codigo del login */}
+    </Fragment>)
+}
+
+export default LoginContainer;

292681dec26bbde8c11f9cc70bf9470c39a3d5fa
JuanAntonaccio <jmantonaccio@gmail.com>
2021-11-01 23:49:22 +0000

    modelo usuario

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nombre = db.Column(db.String(50),unique=False,nullable=False)
+    direccion = db.Column(db.String(100),unique=False, nullable=False)
+    telefono = db.Column(db.String(100),unique=False, nullable=False)
+    documento = db.Column(db.String(30),unique=False, nullable=False)
+    fecha_nac = db.Column(db.DateTime,unique=False,nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Usuario %r>' % self.nombre
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "nombre": self.nombre,
+            "direccion":self.direccion,
+            "telefono":self.telefono,
+            "documento":self.docuemnto,
+            "fecha_nac":self.fecha_nac,
+            "is_active":self.is_active
             # do not serialize the password, its a security breach
         }

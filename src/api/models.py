4d6b966872a11a03cb78297bb58049058bd6cfa0
JuanAntonaccio <jmantonaccio@gmail.com>
2021-11-02 01:02:14 +0000

    esta el modelo de usuario pronto sin chequear

--- src/api/models.py
+++ src/api/models.py
@@ -7,12 +7,12 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
     nombre = db.Column(db.String(50),unique=False,nullable=False)
-    direccion = db.Column(db.String(100),unique=False, nullable=False)
-    telefono = db.Column(db.String(100),unique=False, nullable=False)
-    documento = db.Column(db.String(30),unique=False, nullable=False)
-    fecha_nac = db.Column(db.DateTime,unique=False,nullable=False)
+    direccion = db.Column(db.String(100),unique=False, nullable=True)
+    telefono = db.Column(db.String(100),unique=False, nullable=True)
+    documento = db.Column(db.String(30),unique=False, nullable=True)
+    fecha_nac = db.Column(db.DateTime,unique=False,nullable=True)
 
     def __repr__(self):
         return '<Usuario %r>' % self.nombre

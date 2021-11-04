58408ef4e1de9b2349412e0da1b2ad5da1ab6577
Martín Suárez <ms.prgramador@gmail.com>
2021-11-04 03:00:39 +0000

    quedando

--- src/front/js/component/loginContainer.js
+++ src/front/js/component/loginContainer.js
@@ -12,9 +12,54 @@ const LoginContainer = () => {
 						<small>To our website and buy anything!</small>
 					</div>
 				</div>
-				<div className="col-md-6 col-sm-3 right-side">
+				<div className="col-md-6 col-sm-3 right-side d-flex flex-column justify-content-between">
 					<div className="text-center">
-						<img src="https://i.ibb.co/C1Z8b60/logo-FINAL.png" style={{ width: "50%" }} />
+						<img
+							src="https://res.cloudinary.com/dmrzqrcpq/image/upload/v1635985840/logo_FINAL_tw0rfp.png"
+							style={{ width: "50%" }}
+						/>
+					</div>
+					<div className="inputs m-2">
+						<div className="input-group flex-nowrap">
+							<span className="input-group-text" id="addon-wrapping">
+								📧
+							</span>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Email"
+								aria-label="Email"
+								aria-describedby="addon-wrapping"
+							/>
+						</div>
+						<div className="input-group flex-nowrap">
+							<span className="input-group-text" id="addon-wrapping">
+								🔑
+							</span>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Username"
+								aria-label="Username"
+								aria-describedby="addon-wrapping"
+							/>
+						</div>
+					</div>
+					<div className="options d-flex justify-content-between m-2">
+						<div>
+							<input className="m-1" type="checkbox" id="remember" value="remember" />
+							<label htmlFor="remember">Remember me</label>
+						</div>
+
+						<a href="#">Forgot your password?</a>
+					</div>
+					<div className="buttons d-flex justify-content-between m-2">
+						<button type="button" className="btn btn-primary">
+							Login
+						</button>
+						<button type="button" className="btn btn-primary">
+							Register
+						</button>
 					</div>
 				</div>
 			</div>

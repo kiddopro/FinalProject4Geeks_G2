fbafe63e1bda8cbc729d337a4cb13251857d621f
Martín Suárez <ms.prgramador@gmail.com>
2021-11-04 03:21:34 +0000

    quedando con color

--- src/front/js/component/loginContainer.js
+++ src/front/js/component/loginContainer.js
@@ -5,19 +5,24 @@ const LoginContainer = () => {
 	return (
 		<div id="contenedor" className="container">
 			<div className="row">
-				<div id="left" className="col-md-6 col-sm-3 d-flex left-side justify-content-center align-items-center">
-					<div id="left-side" />
-					<div id="text">
-						<h1>Login</h1>
-						<small>To our website and buy anything!</small>
-					</div>
-				</div>
-				<div className="col-md-6 col-sm-3 right-side d-flex flex-column justify-content-between">
-					<div className="text-center">
+				<div id="left" className="col-md-6 col-sm-6 left-side d-flex align-items-center">
+					{/* <div id="left-side" /> */}
+					<div id="text" className="d-flex flex-column align-items-center text-white">
+						{/* <h1>Login</h1> */}
 						<img
 							src="https://res.cloudinary.com/dmrzqrcpq/image/upload/v1635985840/logo_FINAL_tw0rfp.png"
 							style={{ width: "50%" }}
 						/>
+						<small>To our website and buy anything!</small>
+					</div>
+				</div>
+				<div className="col-md-6 col-sm-6 right-side d-flex flex-column justify-content-between">
+					<div className="title text-center">
+						<h1>Login</h1>
+						{/* <img
+							src="https://res.cloudinary.com/dmrzqrcpq/image/upload/v1635985840/logo_FINAL_tw0rfp.png"
+							style={{ width: "50%" }}
+						/> */}
 					</div>
 					<div className="inputs m-2">
 						<div className="input-group flex-nowrap">

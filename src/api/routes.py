8e286520ddf7c8ff90c4645be4ced22a5b357f30
Martín Suárez <ms.prgramador@gmail.com>
2021-11-06 02:49:52 +0000

    aded login endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,23 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+# login
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    # filtramos los usuarios segun su email y contraseña
+    user = Usuario.query.filter_by(email=email, password=password).first()
+
+    # si no encuentra match en la base de datos retorna el mensaje correspondiente
+    if user is None:
+        return jsonify({"msg": "Bad email or password", "status": 401}), 401
+    else:
+        # de lo contrario se crea un token haciendo referencia a la id del usuario
+        access_token = create_access_token(identity=user.id)
+        # retornamos un json con los datos del token y la id de usuario a la que se le esta asignando el mismo
+        return jsonify({"token": access_token, "user_id": user.id, "msg": "user login in correctly", "status": 200})s

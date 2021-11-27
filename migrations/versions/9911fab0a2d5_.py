943b8da10f79c1221663688498d90f037eec62e7
JuanAntonaccio <jmantonaccio@gmail.com>
2021-11-27 00:45:51 +0000

    cargando mas datos

--- migrations/versions/9911fab0a2d5_.py
+++ migrations/versions/9911fab0a2d5_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 32722be5385d
+Revision ID: 9911fab0a2d5
 Revises: 
-Create Date: 2021-11-26 22:31:00.654883
+Create Date: 2021-11-26 22:57:00.560144
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '32722be5385d'
+revision = '9911fab0a2d5'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -24,7 +24,7 @@ def upgrade():
     sa.Column('marca', sa.String(length=80), nullable=False),
     sa.Column('precio', sa.Float(), nullable=False),
     sa.Column('imagen', sa.String(length=200), nullable=False),
-    sa.Column('descripcion', sa.String(length=100), nullable=False),
+    sa.Column('descripcion', sa.String(length=200), nullable=False),
     sa.Column('categoria', sa.String(length=100), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )

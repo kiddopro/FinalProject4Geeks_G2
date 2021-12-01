1ad1c418c5d28c536d35cbba37cefca2006f59b8
Martín Suárez <ms.prgramador@gmail.com>
2021-12-01 16:47:22 +0000

    obteniendo todos los usuarios

--- src/front/js/pages/admin.js
+++ src/front/js/pages/admin.js
@@ -12,7 +12,18 @@ const Admin = () => {
 				<div className="col-md-12">
 					<h1 className="text-center">Panel de Administrador</h1>
 				</div>
-				<div className="col-md-12">Hola</div>
+				<div className="col-md-12">
+					{store.listaUsuarios.map((item, index) => {
+						return (
+							<div key={index} className="">
+								{item.email}
+								<button onClick={() => console.log(item.id)} type="submit">
+									❌
+								</button>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);

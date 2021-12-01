ae9c76545472ddd40c93e96d9f0b36437fe05627
Martín Suárez <ms.prgramador@gmail.com>
2021-12-01 16:16:53 +0000

    quitado progresso en los toast

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,8 +6,8 @@ const Toast = Swal.mixin({
 	toast: true,
 	position: "top-end",
 	showConfirmButton: false,
-	timer: 1000,
-	timerProgressBar: true,
+	timer: 1500,
+	timerProgressBar: false,
 	didOpen: toast => {
 		toast.addEventListener("mouseenter", Swal.stopTimer);
 		toast.addEventListener("mouseleave", Swal.resumeTimer);

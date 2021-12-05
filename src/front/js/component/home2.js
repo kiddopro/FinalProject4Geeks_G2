bfeaf04b478126343baecef17a2673e8e795514c
Martín Suárez <ms.prgramador@gmail.com>
2021-12-05 06:48:57 +0000

    agregando video

--- src/front/js/component/home2.js
+++ src/front/js/component/home2.js
@@ -2,9 +2,9 @@ import React, { Component, Fragment, useContext, useEffect } from "react";
 
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
+import ReactPlayer from "react-player";
 
 import ProductsContainer from "./productsContainer";
-import videoBG from "../../img/videoo.mp4";
 
 export const Hom2 = props => {
 	const { store, actions } = useContext(Context);
@@ -15,9 +15,14 @@ export const Hom2 = props => {
 	return (
 		<Fragment>
 			<div id="bgVideo">
-				<videoBG loop autoPlay muted>
-					<source src={videoBG} type="video/mp4" />
-				</videoBG>
+				<ReactPlayer
+					url={require("../../img/Nft Video - 74779.mp4")}
+					width="100%"
+					height="100%"
+					playing
+					muted
+					loop
+				/>
 			</div>
 
 			<a name="informacion" />
